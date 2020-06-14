af68b3f5c469abdb596c010d8cfe3ea8ad2cabcd
Jimmy Utterström <jimutt@outlook.com>
2020-06-14 15:01:21 +0200

    Fallback to piano if score uses non-supported instrument (#20)
    * Fallback to piano if score uses non-supported instrument
    
    * Rename Synth Voice to Synth Choir

--- demos/vue-player-demo/src/components/InstrumentControl.vue
+++ demos/vue-player-demo/src/components/InstrumentControl.vue
@@ -5,7 +5,7 @@
       <h4>{{ voice.Name }}</h4>
       <v-select
         class="mb-4"
-        :value="playbackEngine.getPlaybackInstrument(voice.VoiceId)[0]"
+        :value="playbackEngine.getPlaybackInstrument(voice.VoiceId).midiId"
         :items="instruments"
         @change="midiInstrumentId => setPlaybackInstrument(voice, midiInstrumentId)"
       ></v-select>
@@ -26,8 +26,8 @@ export default {
     instruments() {
       if (!this.playbackEngine.availableInstruments) return [];
       return this.playbackEngine.availableInstruments.map(i => ({
-        text: i[1],
-        value: i[0]
+        text: i.name,
+        value: i.midiId
       }));
     }
   },
